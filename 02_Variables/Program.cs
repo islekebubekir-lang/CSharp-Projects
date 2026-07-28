a9df7a00a873a01e7d236a2d70f87c6a469af098
ebubekir işlek <islekebubekir@gmail.com>
2026-07-28 15:53:01 +0300

    Degiskenler örnegi eklendi (flat whıte)

---
+++ 02_Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿/* string yer = "halic kongre merkezi";
+int katilimcisayisi = 20;
+System.Console.WriteLine($"{yer} {katilimcisayisi}");
+ */
+ string ürünadi = "flat white";
+ int ürünadedi = 3;
+ double ürünfiyati = 450.00;
+ bool stoktavarmi = true;
+ System.Console.WriteLine($"{ürünadi} {ürünadedi} {ürünfiyati} {stoktavarmi}");
