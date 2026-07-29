28cf9f5b7f6bea048564a1f5134693b67de53a40
ebubekir işlek <islekebubekir@gmail.com>
2026-07-29 17:09:17 +0300

    .\03_CharacterConstans.csproj

---
+++ 03_CharacterConstantsAndStringLiteralsAndConstants/Program.cs
@@ -0,0 +1,30 @@
+﻿string üyeAdi = "\nSoner\nErdem";
+string üyeDogumYeri = "\tKonya\tMeram";
+string üyeDogumTarihi = "02\\05\\1998";
+bool medeniDurumu = false;
+string anneAdi = @"Halime Erdem";
+string babaAdi = "Ali \"Kemal\" Erdem";
+System.Console.WriteLine($"{üyeAdi}{üyeDogumYeri} {üyeDogumTarihi} {medeniDurumu} {anneAdi} {babaAdi}");
+
+string? giris = Console.ReadLine();
+System.Console.WriteLine(giris);
+
+var input = Console.ReadKey();
+System.Console.WriteLine(input.Key);
+System.Console.WriteLine(input.KeyChar);
+
+string arabaMarkasi = " maserati ";
+System.Console.WriteLine(arabaMarkasi);
+System.Console.WriteLine(arabaMarkasi.Length);
+System.Console.WriteLine(arabaMarkasi.ToUpper());
+System.Console.WriteLine(arabaMarkasi.ToLower());
+System.Console.WriteLine(arabaMarkasi.TrimStart());
+System.Console.WriteLine(arabaMarkasi.TrimEnd());
+System.Console.WriteLine(arabaMarkasi[0]);
+System.Console.WriteLine(arabaMarkasi[1]);
+System.Console.WriteLine(arabaMarkasi[arabaMarkasi.Length - 1]);
+
+
+
+
+
