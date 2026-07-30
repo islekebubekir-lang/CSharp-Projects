100b7978171224d04fce402b2330825cfa0e4d84
ebubekir işlek <islekebubekir@gmail.com>
2026-07-30 18:24:20 +0300

    .\04_Operators.csproj

---
+++ 04_Operators/Program.cs
@@ -0,0 +1,55 @@
+﻿// int A=20, B=10;
+// System.Console.WriteLine(A+B);
+// System.Console.WriteLine(A-B);
+// System.Console.WriteLine(A*B);
+// System.Console.WriteLine(A/B);
+// System.Console.WriteLine(A%B);
+
+// System.Console.WriteLine(A>B);
+// System.Console.WriteLine(A<B);
+// System.Console.WriteLine(A>=B);
+// System.Console.WriteLine(A<=B);
+// System.Console.WriteLine(A==B);
+// System.Console.WriteLine(A!=B);
+
+// System.Console.WriteLine(!(A>B && A<5));
+// System.Console.WriteLine(A<B || B>5);
+
+/* int s=32, k=25;
+System.Console.WriteLine(s+k);
+System.Console.WriteLine(s-k);
+System.Console.WriteLine(s*k);
+System.Console.WriteLine(s/k);
+System.Console.WriteLine(s%k); */
+
+/* int age1=40, age2=40;
+System.Console.WriteLine(age1>age2);
+System.Console.WriteLine(age1<age2);
+System.Console.WriteLine(age1>=age2);
+System.Console.WriteLine(age1<=age2);
+System.Console.WriteLine(age1==age2);
+System.Console.WriteLine(age1!=age2); */
+
+/* int x=7, y=3;
+System.Console.WriteLine(!(x>y && y<1));
+System.Console.WriteLine(y>x || x<10); */
+
+int Aoo1=32,Aoo2=8;
+System.Console.WriteLine(Aoo1*Aoo2); 
+
+int Ioo1=200, Ioo2=100;
+System.Console.WriteLine(Ioo1 < Ioo2);
+System.Console.WriteLine(Ioo1==Ioo2);
+System.Console.WriteLine(Ioo1!=Ioo2); 
+
+int Moo1=30, Moo2=10;
+System.Console.WriteLine(!(Moo1>Moo2 && Moo1>50));
+System.Console.WriteLine(Moo2<Moo1 || Moo1>50);
+
+
+
+
+
+
+
+
