81f6e9c83d48ed3de8ae8fe05312bf28a3c04d4b
ebubekir işlek <islekebubekir@gmail.com>
2026-08-03 20:00:54 +0300

    .\02_Variables.csproj

--- 02_Variables/Program.cs
+++ 02_Variables/Program.cs
@@ -1,9 +1,43 @@
-﻿/* string yer = "halic kongre merkezi";
+﻿string yer = "halic kongre merkezi";
 int katilimcisayisi = 20;
 System.Console.WriteLine($"{yer} {katilimcisayisi}");
- */
- string ürünadi = "flat white";
- int ürünadedi = 3;
- double ürünfiyati = 450.00;
- bool stoktavarmi = true;
- System.Console.WriteLine($"{ürünadi} {ürünadedi} {ürünfiyati} {stoktavarmi}");
+
+string ürünadi = "flat white";
+int ürünadedi = 3;
+double ürünfiyati = 450.00;
+bool stoktavarmi = true;
+System.Console.WriteLine($"{ürünadi} {ürünadedi} {ürünfiyati} {stoktavarmi}");
+ 
+string gidisYeri ="Istanbul";
+string varisYeri ="Ankara";
+string gidisSaati = "22:00";
+string varisSaati = "06:00";
+double biletFiyati = 1500;
+int biletAdedi = 1;
+System.Console.WriteLine($"{gidisYeri} {varisYeri} {gidisSaati} {varisSaati} {biletFiyati} {biletAdedi}");
+
+int rezerveMasa = 5;
+System.Console.WriteLine(rezerveMasa);
+rezerveMasa= 8;
+System.Console.WriteLine(rezerveMasa);
+
+double traşFiyati= 400;
+System.Console.WriteLine(traşFiyati + 200);
+System.Console.WriteLine(traşFiyati*2);
+System.Console.WriteLine(traşFiyati/4);
+System.Console.WriteLine(traşFiyati-200);
+
+string isimSoyisim = "soner Çalişkan";
+string tcKimlikNo ="35676587696";
+string dogumTarihi ="02.06.1997";
+string telNumarasi ="05428793090";
+System.Console.WriteLine($"{isimSoyisim} {tcKimlikNo} {dogumTarihi} {telNumarasi}");
+
+
+
+
+
+
+ 
+
+
