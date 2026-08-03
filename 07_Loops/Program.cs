f52aee258a8a68b34011a1220d7ca721c4e10006
ebubekir işlek <islekebubekir@gmail.com>
2026-08-03 19:50:21 +0300

    .\07_Loops.csproj

---
+++ 07_Loops/Program.cs
@@ -0,0 +1,21 @@
+﻿/* int a = 0;
+while (a<10)
+{
+    System.Console.WriteLine(a);
+    a++;
+} */
+
+/* bool hatMesgulMu=true;
+while (hatMesgulMu)
+{
+    System.Console.WriteLine("Aradiğiniz Kişi Başka Birisiyle Görüşmektedir. ");
+    hatMesgulMu=false;
+} */
+
+ bool adminMi=true;
+while (adminMi)
+{
+    System.Console.WriteLine("Hosgeldiniz. ");
+    adminMi = false;
+}
+
