0ee5ed6980a775723df4a8946444db20381e45b1
ebubekir işlek <islekebubekir@gmail.com>
2026-08-03 20:05:35 +0300

    .\04_Operators.csproj

--- 04_Operators/Program.cs
+++ 04_Operators/Program.cs
@@ -1,38 +1,38 @@
-﻿// int A=20, B=10;
-// System.Console.WriteLine(A+B);
-// System.Console.WriteLine(A-B);
-// System.Console.WriteLine(A*B);
-// System.Console.WriteLine(A/B);
-// System.Console.WriteLine(A%B);
+﻿/*  int A=20, B=10;
+ System.Console.WriteLine(A+B);
+ System.Console.WriteLine(A-B);
+ System.Console.WriteLine(A*B);
+ System.Console.WriteLine(A/B);
+ System.Console.WriteLine(A%B);
 
-// System.Console.WriteLine(A>B);
-// System.Console.WriteLine(A<B);
-// System.Console.WriteLine(A>=B);
-// System.Console.WriteLine(A<=B);
-// System.Console.WriteLine(A==B);
-// System.Console.WriteLine(A!=B);
+ System.Console.WriteLine(A>B);
+ System.Console.WriteLine(A<B);
+ System.Console.WriteLine(A>=B);
+ System.Console.WriteLine(A<=B);
+ System.Console.WriteLine(A==B);
+ System.Console.WriteLine(A!=B);
 
-// System.Console.WriteLine(!(A>B && A<5));
-// System.Console.WriteLine(A<B || B>5);
+ System.Console.WriteLine(!(A>B && A<5));
+ System.Console.WriteLine(A<B || B>5);
 
-/* int s=32, k=25;
+int s=32, k=25;
 System.Console.WriteLine(s+k);
 System.Console.WriteLine(s-k);
 System.Console.WriteLine(s*k);
 System.Console.WriteLine(s/k);
-System.Console.WriteLine(s%k); */
+System.Console.WriteLine(s%k); 
 
-/* int age1=40, age2=40;
+int age1=40, age2=40;
 System.Console.WriteLine(age1>age2);
 System.Console.WriteLine(age1<age2);
 System.Console.WriteLine(age1>=age2);
 System.Console.WriteLine(age1<=age2);
 System.Console.WriteLine(age1==age2);
-System.Console.WriteLine(age1!=age2); */
+System.Console.WriteLine(age1!=age2); 
 
-/* int x=7, y=3;
+int x=7, y=3;
 System.Console.WriteLine(!(x>y && y<1));
-System.Console.WriteLine(y>x || x<10); */
+System.Console.WriteLine(y>x || x<10); 
 
 int Aoo1=32,Aoo2=8;
 System.Console.WriteLine(Aoo1*Aoo2); 
@@ -44,7 +44,31 @@ System.Console.WriteLine(Ioo1!=Ioo2);
 
 int Moo1=30, Moo2=10;
 System.Console.WriteLine(!(Moo1>Moo2 && Moo1>50));
-System.Console.WriteLine(Moo2<Moo1 || Moo1>50);
+System.Console.WriteLine(Moo2<Moo1 || Moo1>50); 
+
+string unvan =@"Müdür/İşe Alim Uzmani";
+string deneyim = " +5 Yil";
+string calismaSekli=" Hibrit";
+System.Console.WriteLine($"{unvan}{deneyim}{calismaSekli}");  */
+/* 
+Console.Write("Lütfen İsim Soyisim Giriniz ");
+string isimSoyisim = System.Console.ReadLine()!;
+System.Console.WriteLine($"Hoşgeldin {isimSoyisim}"); */
+
+/* int sayi1= 50, sayi2=30;
+System.Console.WriteLine(sayi1>sayi2);
+System.Console.WriteLine(sayi1!=sayi2);
+System.Console.WriteLine(sayi1==sayi2);
+System.Console.WriteLine(sayi1<sayi2); */
+
+int sayi1 = 25, sayi2=15;
+System.Console.WriteLine(sayi1<sayi2 && sayi2>5);
+System.Console.WriteLine(sayi2>35 || sayi1>3);
+
+
+
+
+
 
 
 
