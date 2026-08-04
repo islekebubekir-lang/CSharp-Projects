cde00bb21430892b93b3851f4a97dced42fd023e
ebubekir işlek <islekebubekir@gmail.com>
2026-08-04 19:28:11 +0300

    .\07_Loops.csproj

--- 07_Loops/Program.cs
+++ 07_Loops/Program.cs
@@ -12,11 +12,60 @@ while (hatMesgulMu)
     hatMesgulMu=false;
 } */
 
-bool adminMi = true;
+/* bool adminMi = true;
 while (adminMi)
 {
     System.Console.WriteLine("Hosgeldiniz. ");
     adminMi = false;
 }
+System.Console.WriteLine("Bir sayi giriniz.");
+int n = Convert.ToInt32(Console.ReadLine()!);
+int i = 2;
+do
+{
+    System.Console.WriteLine("{0,3}", i);
+    i += 1;
+} while (i <= n); */
+/* 
+int gizliSayi = 42;
+System.Console.WriteLine("Bir sayi giriniz. ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+
+do
+{
+    if (n > gizliSayi)
+    {
+        System.Console.WriteLine("Daha küçük bir sayi giriniz");
+        n = Convert.ToInt32(Console.ReadLine()!);
+    }
+    else if (n < gizliSayi)
+    {
+        System.Console.WriteLine("Daha büyük bir sayi giriniz.");
+        n = Convert.ToInt32(Console.ReadLine()!);
+    }
+    else if (n == gizliSayi)
+    {
+        System.Console.WriteLine("Tebrikler Doğru sayiyi girdiniz.");
+    }
+} while (gizliSayi != n); */
+
+int toplam = 0;
+int sayi;
+do
+{
+    System.Console.WriteLine("Lütfen bir sayi giriniz.");
+    sayi = Convert.ToInt32(Console.ReadLine()!);
+
+    if (sayi > 0)
+    {
+        toplam += sayi;
+
+    }
+    else
+    {
+
+    }
+} while (sayi > 0);
+System.Console.WriteLine("Girdiginiz pozitif sayilarin toplami" + toplam);
 
 
